60c1d73539dc6b68ab54450d9ef93df112f4a40b
adoppp <121878757+adoppp@users.noreply.github.com>
2023-06-17 13:25:36 +0200

    include footer

---
+++ js/index.js
@@ -0,0 +1,15 @@
+new Swiper('.slider', {
+    spaceBetween: 40,
+
+    navigation: {
+        nextEl: '.slider-swiper-button-next',
+        pervEl: '.slider-swiper-button-prev',
+    },
+
+    grabcursor: true,
+   
+    slidesPerView: 3,
+
+    loop: true,
+
+});
