3a14c1136d564d299a4bbf9bb3e6ccfaeb828419
adoppp <121878757+adoppp@users.noreply.github.com>
2023-06-19 03:07:07 +0200

    at and 3 pages

--- js/index.js
+++ js/index.js
@@ -5,11 +5,32 @@ new Swiper('.slider', {
         nextEl: '.slider-swiper-button-next',
         pervEl: '.slider-swiper-button-prev',
     },
-
-    grabСursor: true,
    
     slidesPerView: 3,
 
     loop: true,
 
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+
+            loop: true,
+        },
+            
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+
+            loop: true,
+        },
+
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+
+            loop: true,
+        },
+
+    },
 });
