1c5947f5b9288b25cd87fc4f0a4fa69eb0d24a27
adoppp <121878757+adoppp@users.noreply.github.com>
2023-06-21 13:48:17 +0200

    animated

--- js/index.js
+++ js/index.js
@@ -13,23 +13,16 @@ new Swiper('.slider', {
     breakpoints: {
         320: {
             slidesPerView: 1,
-            spaceBetween: 10,
-
-            loop: true,
         },
             
         768: {
             slidesPerView: 2,
-            spaceBetween: 20,
 
-            loop: true,
         },
 
         1024: {
             slidesPerView: 3,
-            spaceBetween: 30,
 
-            loop: true,
         },
 
     },
